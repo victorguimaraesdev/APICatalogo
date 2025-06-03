2f19188f580c8bc8eeb5a4180f9f79f759cf9a9c
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-06-03 16:55:56 -0300

    Init APICatalogo

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models; 
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "APICatalogo#",
+        Version = "v1"
+    });
+});
+
+string? mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(mySqlConnection,
+ServerVersion.AutoDetect(mySqlConnection)));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "APICatalogo");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
