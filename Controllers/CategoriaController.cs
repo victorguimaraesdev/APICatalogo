148a7128bd0b94e2034c696a195aa758c48dcf51
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-06-17 12:59:00 -0300

    Adicionado CategoriaController

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,46 @@
+using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            return _context.Categorias.ToList();
+        }
+        [HttpGet("{id:int}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+            return categoria != null ? Ok(categoria) : NotFound("Categoria não encontrada");
+        }
+        [HttpPost]
+        public ActionResult Post(Categoria categoria)
+        {
+            if (categoria is null) return BadRequest();
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+        }
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Categoria categoria)
+        {
+            if (id != categoria.CategoriaId) return BadRequest();
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok(categoria);
+        }
+
+   } 
+}
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -22,7 +22,7 @@ namespace APICatalogo.Models
         public string? ImagemUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
-        public int CategoriaId { get; set; }
-        public Categoria? Categoria { get; set; }
+        public int CategoriaId { get; set; } // chave da categoria
+        public Categoria? Categoria { get; set; } //Informações da categoria
     }
 }
