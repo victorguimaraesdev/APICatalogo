d88c19d527626a28b7446d84c39e532836b5f8b9
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-06-19 09:24:14 -0300

    Otimizações ll

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -28,40 +28,26 @@ namespace APICatalogo.Controllers
         [HttpPost]
         public ActionResult Post([FromBody] Produto produto)
         {
-            if (produto is null)
-            {
-                return BadRequest();
-            }
-
+            if (produto is null) return BadRequest();
             _context.Produtos.Add(produto);
             _context.SaveChanges();
-
             return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
         }
         [HttpPut("{id:int}")]
         public ActionResult Put(int id, Produto produto)
         {
-            if (id != produto.ProdutoId)
-            {
-                return BadRequest();
-            }
-
+            if (id != produto.ProdutoId) return BadRequest();
             _context.Entry(produto).State = EntityState.Modified;
             _context.SaveChanges();
-
             return Ok(produto);
         }
         [HttpDelete("{id:int}")]
         public ActionResult Delete(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-            if (produto is null)
-            {
-                return NotFound("Produto não encontrado");
-            }
+            if (produto is null) return NotFound("Produto não encontrado");
             _context.Produtos.Remove(produto);
             _context.SaveChanges();
-
             return Ok(produto);
         }
     }  
